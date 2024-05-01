32605313635bf30c32d5e92670617bb0eac665ac
Vyacheslav Shulika <wildhunter.85@yandex.ru>
2024-05-02 00:02:16 +0300

    recursion after number(fail test)

--- encrypting_instruction.py
+++ encrypting_instruction.py
@@ -1,24 +1,29 @@
 def encrypting(text:str, count = None):
     stack: list = []
-    cript_str: list = []
+    encript_str: list = []
 
-    if len(text) <= 1:
+    if len(text) <= 1 and len(stack) <= 0:
         return text
     else:
-        i = 0
-        for ch_str in text:
+        i: int = 0
+        for ch_str in  text:
             i += 1
-
             if ch_str in '1234567890':
-                stack.append(ch_str)
-                count = "".join(stack)
-                encrypting(text[i:],count)
-            if ch_str not in '[]' and ch_str not in '[]'and ch_str not in '1234567890':
-                cript_str.append(ch_str)
-                
-            #encript_str = cript_str.pop()
-            #end_str = "".join(cript_str) * int(count)
-        return "".join(cript_str) * int(count)
+               stack.append(ch_str)
+               count = stack.pop()
+               encrypting(text[i+1:-1], count)
+            if ch_str not in'[]' and ch_str not in '1234567890':
+               encript_str.append(ch_str)
+            if ch_str in ']':
+               str_count = encript_str.pop()
+               #encript_str = []
+               end_str: str = str_count * int(count)
+               encript_str.append(end_str)
+               end_total_str = "".join(encript_str)
+               #encrypting(text[i:])
+        return end_total_str
+
+       # return "".join(cript_str) * int(count)
                 
                
 
